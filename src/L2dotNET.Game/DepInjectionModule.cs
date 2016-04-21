b211c5b418a2ad1f243e20223a2df8d0caded91a
Elfocrash <elfocrash@gmail.com>
2016-04-21 09:38:15 +0100

    Added Ninject and a Dependancy Module

---
+++ src/L2dotNET.Game/DepInjectionModule.cs
@@ -0,0 +1,25 @@
+﻿using L2dotNET.Repositories;
+using L2dotNET.Repositories.Contracts;
+using L2dotNET.Services;
+using L2dotNET.Services.Contracts;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2dotNET.Game
+{
+    public class DepInjectionModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IPlayerService>().To<PlayerService>();
+
+
+            Bind<IPlayerRepository>().To<PlayerRepository>();
+            Bind<IUnitOfWork>().To<UnitOfWork>();
+        }
+    }
+}
