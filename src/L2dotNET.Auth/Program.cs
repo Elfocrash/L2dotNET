a4117ebedd29a70892012f368d97d754e6209ee8
Elfocrash <elfocrash@gmail.com>
2016-04-22 21:45:48 +0000

    Forgot to include those to the project

---
+++ src/L2dotNET.Auth/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using L2dotNET.Auth.data;
+using L2dotNET.Auth.gscommunication;
+using L2dotNET.Auth.managers;
+using Ninject;
+
+namespace L2dotNET.Auth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginServer.Kernel = new StandardKernel(new DepInjectionModule());
+            LoginServer server = LoginServer.Kernel.Get<LoginServer>();
+            server.Start();
+            Process.GetCurrentProcess().WaitForExit();
+        }
+
+      
+    }
+}
