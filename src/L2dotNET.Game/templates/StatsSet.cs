3d3b36cf4630f2bca29f2e9f12bed26f86abdce9
Mart <martlegion@hotmail.com>
2016-05-10 18:51:58 -0300

    Added "defaultValue" parameter to all current GetT methods. Fixed the logic to return the "defaultValue", updated to return only if conversion fails (Note: still need to validate if the key exists on dictionary) Added generic Get<T> to get values from dictionary (Note: using Reflection, maybe has some performance issue but make the code more clean. Added to future testing purposes. Just remove it if has no benefits) (Note 2: Included logger to check if key exists and if conversion failed)

--- src/L2dotNET.Game/templates/StatsSet.cs
+++ src/L2dotNET.Game/templates/StatsSet.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,8 @@ namespace L2dotNET.Game.templates
 {
     public class StatsSet : Dictionary<string, object>
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(StatsSet));
+
         public StatsSet() : base() { }
 
         public StatsSet(int size) : base(size) { }
@@ -67,12 +71,30 @@ namespace L2dotNET.Game.templates
             return toReturn;
         }
 
+        public bool GetBool(string key, bool defaultVal)
+        {
+            var val = base[key].ToString();
+            bool toReturn;
+            if (bool.TryParse(val, out toReturn))
+                return toReturn;
+            return defaultVal;
+        }
+
         public byte GetByte(string key)
         {
             var val = base[key].ToString();
             byte toReturn;
             byte.TryParse(val, out toReturn);
             return toReturn;
+        }
+
+        public byte GetByte(string key, byte defaultVal)
+        {
+            var val = base[key].ToString();
+            byte toReturn;
+            if (byte.TryParse(val, out toReturn))
+                return toReturn;
+            return defaultVal;
         }
 
         public double GetDouble(string key)
@@ -85,6 +107,10 @@ namespace L2dotNET.Game.templates
 
         public double GetDouble(string key, double defaultVal)
         {
+            var val = base[key].ToString();
+            double toReturn;
+            if (double.TryParse(val, out toReturn))
+                return toReturn;
             return defaultVal;
         }
 
@@ -94,6 +120,15 @@ namespace L2dotNET.Game.templates
             float toReturn;
             float.TryParse(val, out toReturn);
             return toReturn;
+        }
+
+        public float GetFloat(string key, float defaultVal)
+        {
+            var val = base[key].ToString();
+            float toReturn;
+            if (float.TryParse(val, out toReturn))
+                return toReturn;
+            return defaultVal;
         }
 
         public int GetInt(string key)
@@ -106,6 +141,10 @@ namespace L2dotNET.Game.templates
 
         public int GetInt(string key, int defaultVal)
         {
+            var val = base[key].ToString();
+            int toReturn;
+            if (int.TryParse(val, out toReturn))
+                return toReturn;
             return defaultVal;
         }
 
@@ -117,7 +156,55 @@ namespace L2dotNET.Game.templates
 
         public string GetString(string key, string defaultVal)
         {
-            return defaultVal;
+            var val = base[key].ToString();
+            if (string.IsNullOrWhiteSpace(val))
+                return defaultVal;
+            return val;
+        }
+
+        public T Get<T>(string key, T defaultValue = default(T)) where T : struct, IConvertible
+        {
+            //check if the dictionary contains the key
+            if (base.ContainsKey(key))
+            {
+                string value = base[key].ToString();
+
+                // find the TryParse method.
+                MethodInfo parseMethod = typeof(T).GetMethod("TryParse",
+                                                             // We want the public static one
+                                                             BindingFlags.Public | BindingFlags.Static,
+                                                             Type.DefaultBinder,
+                                                             // where the arguments are (string, out T)
+                                                             new[] { typeof(string), typeof(T).MakeByRefType() },
+                                                             null);
+
+                if (parseMethod == null)
+                {
+                    // You need to know this so you can parse manually
+                    log.Error($"'{ typeof(T).FullName }' doesn't have a 'TryParse(string s, out { typeof(T).FullName } result)' function! The function will return the 'defaultValue' parameter.");
+                    return defaultValue;
+                }
+
+                // create the parameter list for the function call
+                object[] args = new object[] { value, default(T) };
+
+                // and then call the function.
+                if ((bool)parseMethod.Invoke(null, args))
+                    return (T)args[1]; // if it returned true, returns converted value
+                else
+                {
+                    log.Info($"Conversion of key '{ key }' failed! Cannot convert value '{ value }' to '{ typeof(T).FullName }'!");
+                    return default(T); // if it returned false, returns default value of 'T'
+                }
+            }
+            else
+            {
+                log.Info($"Key '{ key }' was not found in the dictionary! The function will return the 'defaultValue' parameter.");
+                //if key doesn't exists,
+                //returns the defaultValue var,
+                //when not specified returns the default value of 'T'
+                return defaultValue;
+            }
         }
 
 
