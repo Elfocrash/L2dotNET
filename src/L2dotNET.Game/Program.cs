d69fe41f1b419c66b142b32b363cc8061f5d0cd7
Mart <martlegion@hotmail.com>
2016-05-10 18:58:09 -0300

    Temporary fix. Need a better workaround to fix the Culture conversion issues. (Note: parsing error when reading "." in Latin cultures from XML files)

--- src/L2dotNET.Game/Program.cs
+++ src/L2dotNET.Game/Program.cs
@@ -11,10 +11,19 @@ namespace L2dotNET.Game
         
         static void Main(string[] args)
         {
+            SetNumberDecimalSeparator();
             GameServer.Kernel = new StandardKernel(new DepInjectionModule());
             GameServer server = GameServer.Kernel.Get<GameServer>();
             server.Start();
             Process.GetCurrentProcess().WaitForExit();
+        }
+
+        //TODO: Temporary fix. Need a better workaround to fix the Culture conversion issues. (Note: parsing error when reading "." in Latin cultures from XML files)
+        private static void SetNumberDecimalSeparator()
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
         }
     }
 }
