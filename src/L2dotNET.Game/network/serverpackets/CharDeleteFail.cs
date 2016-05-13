8a696e3e09ac8b132df6f23343df02b6dad0357d
Mart <martlegion@hotmail.com>
2016-05-13 15:43:27 -0300

    Added CharDeleteFail / CharDeleteOk

---
+++ src/L2dotNET.Game/network/serverpackets/CharDeleteFail.cs
@@ -0,0 +1,25 @@
+﻿namespace L2dotNET.GameService.network.l2send
+{
+    class CharDeleteFail : GameServerNetworkPacket
+    {
+        public enum CharDeleteFailReason
+        {
+            DELETION_FAILED = 1,
+			YOU_MAY_NOT_DELETE_CLAN_MEMBER = 2,
+			CLAN_LEADERS_MAY_NOT_BE_DELETED = 3
+        }
+
+        CharDeleteFailReason _reason;
+
+        public CharDeleteFail(CharDeleteFailReason reason)
+        {   
+            _reason = reason;
+        }
+
+        protected internal override void write()
+        {
+            writeC(0x24);
+            writeD((int)_reason);
+        }
+    }
+}
