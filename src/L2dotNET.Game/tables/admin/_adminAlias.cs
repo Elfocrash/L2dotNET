d7c9edf8d55920568b951eaa79801e8fd3105e47
Mart <martlegion@hotmail.com>
2016-06-05 09:23:18 -0300

    Deleted to Sync

--- src/L2dotNET.Game/tables/admin/_adminAlias.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace L2dotNET.GameService.tables.admin
-{
-    public abstract class _adminAlias
-    {
-        protected internal abstract void use(L2Player admin, string alias);
-
-        public string cmd;
-    }
-}
