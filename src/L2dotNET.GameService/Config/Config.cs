ec442da33268c2ee97b03db7e3de45242cdf80ec
Mart <martlegion@hotmail.com>
2016-06-19 12:49:22 -0300

    Splitted json files

--- src/L2dotNET.GameService/Config/Config.cs
+++ src/L2dotNET.GameService/Config/Config.cs
@@ -31,7 +31,14 @@ namespace L2dotNET.GameService.Config
         public void Initialize()
         {
             serverConfig = JsonConvert.DeserializeObject<ServerConfig>(File.ReadAllText(@"config\server.json"));
-            gameplayConfig = JsonConvert.DeserializeObject<GameplayConfig>(File.ReadAllText(@"config\gameplay.json"));
+            gameplayConfig = new GameplayConfig();
+            gameplayConfig.Server = JsonConvert.DeserializeObject<Server2>(File.ReadAllText(@"config\gameplay.json"));
+            gameplayConfig.Clan = JsonConvert.DeserializeObject<Clan>(File.ReadAllText(@"config\clan.json"));
+            gameplayConfig.Event = JsonConvert.DeserializeObject<Event>(File.ReadAllText(@"config\events.json"));
+            gameplayConfig.Login = JsonConvert.DeserializeObject<Login>(File.ReadAllText(@"config\loginserver.json"));
+            gameplayConfig.Npc = JsonConvert.DeserializeObject<Npc>(File.ReadAllText(@"config\npcs.json"));
+            gameplayConfig.Player = JsonConvert.DeserializeObject<Player>(File.ReadAllText(@"config\player.json"));
+            gameplayConfig.Siege = JsonConvert.DeserializeObject<Siege>(File.ReadAllText(@"config\siege.json"));
         }
     }
 }
