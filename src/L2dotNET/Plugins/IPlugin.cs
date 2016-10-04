0d40566593e9baab5c6bc57f3a6a30d704bc4200
Elfocrash <elfocrash@gmail.com>
2016-10-04 14:12:21 +0100

    Changed some documentation and added the onItemEquip method in the plugin interface.

--- src/L2dotNET/Plugins/IPlugin.cs
+++ src/L2dotNET/Plugins/IPlugin.cs
@@ -1,19 +1,31 @@
-﻿using L2dotNET.model.player;
+﻿using L2dotNET.model.items;
+using L2dotNET.model.player;
 
 namespace L2dotNET.Plugins
 {
     public interface IPlugin
     {
         /// <summary>
-        ///     This function will be called on plugin initialization.
+        ///     This method will be called on plugin initialization.
         /// </summary>
         void OnEnable(PluginContext context);
 
         /// <summary>
-        ///     This function will be called when the plugin will be disabled.s
+        ///     This method will be called when the plugin will be disabled.
         /// </summary>
         void OnDisable();
 
+        /// <summary>
+        ///     This method will be called when the player logs in.
+        /// </summary>
+        /// <param name="player"></param>
         void OnLogin(L2Player player);
+
+        /// <summary>
+        ///     This method will be called when a player equips an item.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="item"></param>
+        void OnItemEquip(L2Player player, L2Item item);
     }
 }
