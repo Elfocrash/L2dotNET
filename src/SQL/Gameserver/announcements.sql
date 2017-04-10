0a84562f544eef1deb411095f9027c9dd9148ba3
Marcos Teixeira <martlegion@hotmail.com>
2017-04-10 19:48:20 -0300

    Removed deprecated .sql files/folder Added folder with engine.dll client fix for W10 (need a better location to put this) (thanks Vegax)

--- src/SQL/Gameserver/announcements.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `announcements`;
-CREATE TABLE `announcements` (
-  `id` int(11) NOT NULL auto_increment,
-  `Text` varchar(200) DEFAULT NULL,
-  `type` int(2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
