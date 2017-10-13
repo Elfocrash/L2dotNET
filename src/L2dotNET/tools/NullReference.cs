69dee82ed9a5154603e5cb94c0e00d4f230b575c
gio-abashidze <george.abashidze@hotmail.com>
2017-10-13 22:43:23 +0400

    fixed gameserver crash
    when obj was null there was nullreference exception, i added method which are getting type from object or variable, even if its null or not

---
+++ src/L2dotNET/tools/NullReference.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2dotNET.tools
+{
+    public class NullReference
+    {
+        public static string GetTypeLower<T>(T obj)
+        {
+            Type t;
+            if (obj == null)
+                t = typeof(T);
+            else
+                t = obj.GetType();
+            return t.Name.ToLower();
+        }
+    }
+}
