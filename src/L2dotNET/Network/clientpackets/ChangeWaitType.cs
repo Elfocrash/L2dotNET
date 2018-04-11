cbb1820e1414e7ec9ae2b0a8c6db9d5ad45cc2de
Windex- <eoothewise@gmail.com>
2018-04-10 20:29:17 -0400

    Sit/Stand
    Added base functionality for sit/stand

---
+++ src/L2dotNET/Network/clientpackets/ChangeWaitType.cs
@@ -0,0 +1,41 @@
+﻿using L2dotNET.Models.player;
+using log4net;
+
+namespace L2dotNET.Network.clientpackets
+{
+    class ChangeWaitType : PacketBase
+    {
+        private readonly GameClient _client;
+        private readonly int _standType;
+
+        public ChangeWaitType(Packet packet, GameClient client)
+        {
+            _client = client;
+            _standType = packet.ReadInt();
+        }
+
+        public override void RunImpl()
+        {
+            L2Player player = _client.CurrentPlayer;
+
+            //TODO: Chair/Mount Logic
+            switch (_standType)
+            {
+                case 0:
+                    player.Sit();
+                    break;
+                case 1:
+                    player.Stand();
+                    break;
+                case 2:
+                    //Fake Death
+                    break;
+                case 3:
+                    //Stop Fake Death
+                    break;
+            }
+            
+        }
+    }
+}
+
