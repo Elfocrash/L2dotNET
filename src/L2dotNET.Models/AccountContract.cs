19235bbb23e5520b5648a8b1e39770ee5c8cbce8
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-06-28 15:52:49 +0300

    Changed account model

--- src/L2dotNET.Models/AccountContract.cs
+++ src/L2dotNET.Models/AccountContract.cs
@@ -1,22 +1,28 @@
-﻿namespace L2dotNET.DataContracts
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace L2dotNET.DataContracts
 {
+    [Table("Accounts")]
     public class AccountContract
     {
+        [Key]
+        public int AccountId { get; set; }
+
         public string Login { get; set; }
 
-        public string Password { get; set; }
+        public byte[] Password { get; set; }
 
-        public long LastActive { get; set; }
+        public DateTime LastActive { get; set; }
 
         public int AccessLevel { get; set; }
 
-        public int LastServer { get; set; }
+        public byte LastServer { get; set; }
 
         public AccountContract()
         {
-            Login = string.Empty;
-            Password = string.Empty;
-            LastActive = 0;
+            LastActive = DateTime.Now;
             AccessLevel = 0;
             LastServer = 1;
         }
