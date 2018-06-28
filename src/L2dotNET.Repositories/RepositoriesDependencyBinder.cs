bfc634bd98e425d388f1dada7171e4de539d2455
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-06-28 15:11:15 +0300

    Dependency builder for repositories project

---
+++ src/L2dotNET.Repositories/RepositoriesDependencyBinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using L2dotNET.Repositories.Abstract;
+using L2dotNET.Repositories.Abstract.Contracts;
+using L2dotNET.Repositories.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace L2dotNET.Repositories
+{
+    public static class RepositoriesDependencyBinder
+    {
+        public static void Bind(IServiceCollection provider)
+        {
+            provider.AddSingleton(typeof(ICrudRepository<>), typeof(CrudRepositoryBase<>));
+
+            provider.AddSingleton<IAccountRepository, AccountRepository>();
+            provider.AddSingleton<IEtcItemRepository, EtcItemRepository>();
+            provider.AddSingleton<IArmorRepository, ArmorRepository>();
+        }
+    }
+}
