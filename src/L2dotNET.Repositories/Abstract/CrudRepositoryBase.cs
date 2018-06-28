deac6c166a2766c0d2ede38472e7b588ddd0ca63
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-06-28 15:11:00 +0300

    Added crud repositories

---
+++ src/L2dotNET.Repositories/Abstract/CrudRepositoryBase.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using L2dotNET.Logging.Abstraction;
+using L2dotNET.Repositories.Utils;
+using PeregrineDb;
+
+namespace L2dotNET.Repositories.Abstract
+{
+    public class CrudRepositoryBase<T> : ICrudRepository<T> where T: class
+    {
+        private static readonly ILog Log = LogProvider.GetCurrentClassLogger();
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            try
+            {
+                using (IDatabase database = ConnectionFactory.Open())
+                {
+                    return await database.GetAllAsync<T>();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Method: {nameof(GetAll)}. Message: '{ex.Message}'");
+                throw;
+            }
+        }
+
+        public async Task<T> GetById(object id)
+        {
+            try
+            {
+                using (IDatabase database = ConnectionFactory.Open())
+                {
+                    return await database.GetAsync<T>(id);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Method: {nameof(GetById)}. Message: '{ex.Message}'");
+                throw;
+            }
+        }
+
+        public async Task<int?> Add(T model)
+        {
+            try
+            {
+                using (IDatabase database = ConnectionFactory.Open())
+                {
+                    return await database.InsertAsync<int?>(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Method: {nameof(Add)}. Message: '{ex.Message}'");
+                throw;
+            }
+        }
+
+        public async void Update(T model)
+        {
+            try
+            {
+                using (IDatabase database = ConnectionFactory.Open())
+                {
+                    await database.UpdateAsync(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Method: {nameof(Update)}. Message: '{ex.Message}'");
+                throw;
+            }
+        }
+
+        public async void Delete(T model)
+        {
+            try
+            {
+                using (IDatabase database = ConnectionFactory.Open())
+                {
+                    await database.DeleteAsync(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Method: {nameof(Delete)}. Message: '{ex.Message}'");
+                throw;
+            }
+        }
+    }
+}
---
+++ src/L2dotNET.Repositories/Abstract/ICrudRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace L2dotNET.Repositories.Abstract
+{
+    public interface ICrudRepository<T> where T: class
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> GetById(object id);
+
+        Task<int?> Add(T model);
+
+        void Update(T model);
+
+        void Delete(T model);
+    }
+}
