331a745ab7fc6a23c5efbe6832edec3228daa34c
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-06-28 15:53:38 +0300

    Added connection factory

---
+++ src/L2dotNET.Repositories/Utils/ConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+using PeregrineDb;
+using PeregrineDb.Databases;
+
+namespace L2dotNET.Repositories.Utils
+{
+    static class ConnectionFactory
+    {
+        public static IDatabase Open()
+        {
+            // TODO: Add configuration here
+            SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            return new DefaultDatabase(connection, PeregrineConfig.SqlServer2012);
+        }
+    }
+}
