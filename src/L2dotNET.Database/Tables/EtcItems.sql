a5a80828784421dc87dcc1ba85c43543e53ff3e7
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-07-01 17:20:57 +0300

    Added etc item sql data script

--- src/L2dotNET.Database/Tables/EtcItems.sql
+++ src/L2dotNET.Database/Tables/EtcItems.sql
@@ -2,18 +2,16 @@
 (
 	[EtcItemId] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(70) NOT NULL, 
-    [Crystallizable] BIT NOT NULL, 
     [ItemType] TINYINT NOT NULL, 
-    [Weight] INT NOT NULL, 
-    [ConsumeType] TINYINT NOT NULL, 
+    [Weight] INT NOT NULL,
     [CrystalType] TINYINT NOT NULL, 
     [Duration] INT NOT NULL, 
     [Price] INT NOT NULL, 
-    [CrystalCount] INT NOT NULL, 
+    [CrystalCount] INT NOT NULL,
+    [Crystallizable] BIT NOT NULL, 
     [Sellable] BIT NOT NULL, 
     [Dropable] BIT NOT NULL, 
     [Destroyable] BIT NOT NULL, 
     [Tradeable] BIT NOT NULL, 
-    [OldName] VARCHAR(70) NOT NULL, 
-    [OldType] TINYINT NOT NULL
+    [Stackable] BIT NOT NULL
 )
