405b5b1d39dfe2bbfaac1eb1b399392393b5e4a2
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-07-02 16:36:12 +0300

    Console command refactoring

--- src/L2dotNET/ConsoleCommand/Commands/PlayersCount.cs
+++ src/L2dotNET/ConsoleCommand/Commands/PlayersCount.cs
@@ -4,6 +4,12 @@ namespace L2dotNET.ConsoleCommand.Commands
 {
     class PlayersCount : Command
     {
+        public PlayersCount()
+        {
+            Name = "players-count";
+            Description = "Gets total players on the server";
+        }
+
         public override void Execute(string param)
         {
             Console.WriteLine("Count - " + World.L2World.Instance.GetPlayers().Count);
--- src/L2dotNET/ConsoleCommand/ConsoleCommand.cs
+++ src/L2dotNET/ConsoleCommand/ConsoleCommand.cs
@@ -1,37 +1,80 @@
 ﻿using L2dotNET.ConsoleCommand.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace L2dotNET.ConsoleCommand
 {
     public class ConsoleCommandController
     {
-        public bool isWorkConsoleEnter = true;
-        public void Launch()
-        {
-            Thread thread = new Thread(EnterCommand);
-            thread.Start();
-        }
-
-        Dictionary<string, Command> commands = new Dictionary<string, Command>();
-
-        void EnterCommand()
-        {
-            while (isWorkConsoleEnter)
-            {
-                string key = Console.ReadLine();
-                if (commands.ContainsKey(key))
-                {
-                    commands[key].Execute();
-                }
-            }
-        }
+        private readonly ICollection<Command> _commands;
+        private Thread _commandThread;
 
         public ConsoleCommandController()
         {
-            commands.Add("ObjectsCount", new ObjectsCount());
-            commands.Add("PlayersCount", new PlayersCount());
+            _commands = new List<Command>
+            {
+                new ObjectsCount(),
+                new PlayersCount()
+            };
+        }
+
+        public void Start()
+        {
+            if (_commandThread != null)
+            {
+                return;
+            }
+
+            _commandThread = new Thread(EnterCommand)
+                {
+                    Priority = ThreadPriority.Lowest,
+                    IsBackground = true
+                };
+
+            _commandThread.Start();
+        }
+
+        public void Stop()
+        {
+            if (_commandThread == null)
+            {
+                return;
+            }
+
+            _commandThread.Abort();
+            _commandThread = null;
+        }
+
+        private void EnterCommand()
+        {
+            Console.WriteLine("Command engine is running. Type help to get the info about available commands.");
+            while (true)
+            {
+                string key = Console.ReadLine()?.ToLower();
+
+                if (key == "help")
+                {
+                    Console.WriteLine("Available commands:");
+                    _commands.Select(x => $"{x.Name} - {x.Description}")
+                        .ToList()
+                        .ForEach(Console.WriteLine);
+                }
+                else
+                {
+                    Command cmd = _commands.FirstOrDefault(x => x.Name.ToLower() == key);
+
+                    if (cmd != null)
+                    {
+                        cmd.Execute();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unrecognized command");
+                    }
+                }
+            }
         }
     }
 }
