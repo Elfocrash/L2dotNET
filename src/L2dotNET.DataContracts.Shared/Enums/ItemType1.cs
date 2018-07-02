46361b53c91fa0608bfaa75adef6852ec6e90ebb
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-07-03 02:43:28 +0300

    EtcItem, Armor, BodyPartType refactoring

---
+++ src/L2dotNET.DataContracts.Shared/Enums/ItemType1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L2dotNET.DataContracts.Shared.Enums
+{
+    public enum ItemType1
+    {
+        WeaponRingEarringNecklace = 0,
+        ShieldArmor = 1,
+        ItemQuestitemAdena = 4,
+    }
+}
---
+++ src/L2dotNET.DataContracts.Shared/Enums/ItemType2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L2dotNET.DataContracts.Shared.Enums
+{
+    public enum ItemType2
+    {
+        Weapon = 0,
+        ShieldArmor = 1,
+        Accessory = 2,
+        Quest = 3,
+        Money = 4,
+        Other = 5,
+    }
+}
