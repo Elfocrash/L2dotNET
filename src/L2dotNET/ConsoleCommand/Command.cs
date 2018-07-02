95a5400439cc50e40d6b8687f9c568415516c12f
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-07-02 16:36:12 +0300

    Console command refactoring

--- src/L2dotNET/ConsoleCommand/Command.cs
+++ src/L2dotNET/ConsoleCommand/Command.cs
@@ -2,6 +2,10 @@
 {
     abstract class Command
     {
+        public string Name { get; protected set; }
+
+        public string Description { get; protected set; }
+
         public abstract void Execute(string param = "");
     }
 }
--- src/L2dotNET/ConsoleCommand/Commands/ObjectsCount.cs
+++ src/L2dotNET/ConsoleCommand/Commands/ObjectsCount.cs
@@ -4,6 +4,12 @@ namespace L2dotNET.ConsoleCommand.Commands
 {
     class ObjectsCount : Command
     {
+        public ObjectsCount()
+        {
+            Name = "objects-count";
+            Description = "Gets total number of objects on the server";
+        }
+
         public override void Execute(string param)
         {
             Console.WriteLine("Count - " + World.L2World.Instance.GetObjects().Count);
