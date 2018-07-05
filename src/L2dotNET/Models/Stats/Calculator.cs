52e56ff79cabd375a399c0db2eb2851dacc44ae5
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-07-05 23:57:47 +0300

    CharacterStat, Calculator refactoring.

--- src/L2dotNET/Models/Stats/Calculator.cs
+++ src/L2dotNET/Models/Stats/Calculator.cs
@@ -1,84 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
+using L2dotNET.DataContracts.Shared.Enums;
 
 namespace L2dotNET.Models.Stats
 {
     public class Calculator
     {
-        private static readonly Func[] EmptyFuncs = new Func[0];
+        public int Size => _size;
 
-        private Func[] _functions;
+        private int _size;
+        private readonly ConcurrentBag<StatFunction> _functions;
 
         public Calculator()
         {
-            _functions = EmptyFuncs;
+            _functions = new ConcurrentBag<StatFunction>();
+            _size = 0;
         }
 
-        public Calculator(Calculator calculator)
+        public static Calculator[] GetCalculatorsForStats()
         {
-            _functions = calculator._functions;
-        }
+            Calculator[] calculators = new Calculator[Enum.GetNames(typeof(CharacterStatId)).Length];
 
-        public int Size => _functions.Length;
-
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        public void AddFunc(Func func)
-        {
-            var funcs = _functions;
-            var tmp = new Func[funcs.Length + 1];
-
-            var order = func.Order;
-            int i;
-            for (i = 0; i < funcs.Length && order >= funcs[i].Order; i++)
-                tmp[i] = funcs[i];
-
-            tmp[i] = func;
-
-            for (; i < funcs.Length; i++)
-                tmp[i + 1] = funcs[i];
-
-            _functions = tmp;
-        }
-
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        public void RemoveFunc(Func func)
-        {
-            var funcs = _functions;
-            var tmp = new Func[funcs.Length - 1];
-            
-            int i;
-            for (i = 0; i < funcs.Length && func != funcs[i]; i++)
-                tmp[i] = funcs[i];
-
-            if (i == funcs.Length)
-                return;
-
-            for (; i < funcs.Length; i++)
-                tmp[i - 1] = funcs[i];
-
-            _functions = tmp.Length == 0 ? EmptyFuncs : tmp;
-        }
-
-        [MethodImpl(MethodImplOptions.Synchronized)]
-        public List<Stat> RemoveOwner(object owner)
-        {
-            List<Stat> modifiedStats = new List<Stat>();
-            foreach (var function in _functions)
+            for (int i = 0; i < calculators.Length; i++)
             {
-                if (function.FuncOwner == owner)
-                {
-                    modifiedStats.Add(function.Stat);
-                    RemoveFunc(function);
-                }
+                calculators[i] = new Calculator();
             }
-            return modifiedStats;
+
+            return calculators;
         }
 
-        public void Calculate(Env env)
+        public void AddFunc(StatFunction func)
         {
-            foreach (var function in _functions)
+            Interlocked.Increment(ref _size);
+            _functions.Add(func);
+        }
+
+        public void Calculate(StatFunctionEnvironment statFuncEnv)
+        {
+            foreach (StatFunction function in _functions.OrderBy(x => x.Order))
             {
-                function.Calculate(env);
+                function.Calculate(statFuncEnv);
             }
         }
     }
