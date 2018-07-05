0946a446ea4c481c890a2e0e00abab7b573749b5
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-07-05 18:34:18 +0300

    Fixed characters db

--- src/L2dotNET.Models/Contracts/CharacterContract.cs
+++ src/L2dotNET.Models/Contracts/CharacterContract.cs
@@ -83,9 +83,9 @@ namespace L2dotNET.DataContracts
 
         public DateTime? LastAccess { get; set; }
 
-        public int PunishLevel { get; set; }
+        public int? PunishLevel { get; set; }
 
-        public int PunishTimer { get; set; }
+        public DateTime? PunishTime { get; set; }
 
         public int PowerGrade { get; set; }
 
