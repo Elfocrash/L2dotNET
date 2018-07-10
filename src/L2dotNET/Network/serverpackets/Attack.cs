a138b06b9098141317f41be5d92f337407813c4a
Andrei Ledauski <andrei.ledauski@itechart-group.com>
2018-07-10 13:33:32 +0300

    Reworked Attack and Hit classes

--- src/L2dotNET/Network/serverpackets/Attack.cs
+++ src/L2dotNET/Network/serverpackets/Attack.cs
@@ -1,67 +1,47 @@
-﻿using L2dotNET.Models;
+﻿using System.Collections.Generic;
+using L2dotNET.Models;
 
 namespace L2dotNET.Network.serverpackets
 {
     class Attack : GameserverPacket
     {
-        protected int AttackerObjId;
-        public bool Soulshot;
-        public int Grade;
-        private readonly int _x;
-        private int _tx;
-        private readonly int _y;
-        private int _ty;
-        private readonly int _z;
-        private int _tz;
-        private Hit[] _hits;
+        public List<Hit> Hits { get; set; }
 
-        public Attack(L2Character player, L2Object target, bool ss, int grade)
+        private readonly int _attackerId;
+        private readonly int _x;
+        private readonly int _y;
+        private readonly int _z;
+
+        public Attack(L2Character player, params Hit[] hits)
         {
-            AttackerObjId = player.ObjectId;
-            Soulshot = ss;
-            Grade = grade;
+            _attackerId = player.ObjectId;
             _x = player.X;
             _y = player.Y;
             _z = player.Z;
-            _tx = target.X;
-            _ty = target.Y;
-            _tz = target.Z;
-            _hits = new Hit[0];
-        }
-
-        public void AddHit(int targetId, int damage, bool miss, bool crit, bool shld)
-        {
-            int pos = _hits.Length;
-            Hit[] tmp = new Hit[pos + 1];
-
-            for (int i = 0; i < _hits.Length; i++)
-                tmp[i] = _hits[i];
-
-            tmp[pos] = new Hit(targetId, damage, miss, crit, shld, Soulshot, Grade);
-            _hits = tmp;
+            Hits = new List<Hit>(hits);
         }
 
         public bool HasHits()
         {
-            return _hits.Length > 0;
+            return Hits.Count > 0;
         }
 
         public override void Write()
         {
             WriteByte(0x05);
-            WriteInt(AttackerObjId);
-            WriteInt(_hits[0].TargetId);
-            WriteInt(_hits[0].Damage);
-            WriteByte(_hits[0].Flags);
+            WriteInt(_attackerId);
+            WriteInt(Hits[0].TargetId);
+            WriteInt(Hits[0].Damage);
+            WriteByte(Hits[0].Flags);
             WriteInt(_x);
             WriteInt(_y);
             WriteInt(_z);
-            WriteShort((short)(_hits.Length - 1));
-            for (int i = 1; i < _hits.Length; i++)
+            WriteShort((short)(Hits.Count - 1));
+            for (int i = 1; i < Hits.Count; i++)
             {
-                WriteInt(_hits[i].TargetId);
-                WriteInt(_hits[i].Damage);
-                WriteByte(_hits[i].Flags);
+                WriteInt(Hits[i].TargetId);
+                WriteInt(Hits[i].Damage);
+                WriteByte(Hits[i].Flags);
             }
         }
     }
--- src/L2dotNET/Network/serverpackets/Hit.cs
+++ src/L2dotNET/Network/serverpackets/Hit.cs
@@ -2,23 +2,51 @@ namespace L2dotNET.Network.serverpackets
 {
     public class Hit
     {
-        public int TargetId;
-        public int Damage;
-        public int Flags;
+        private const int HITFLAG_USESS = 0x10;
+        private const int HITFLAG_CRITICAL = 0x20;
+        private const int HITFLAG_SHIELD = 0x40;
+        private const int HITFLAG_MISS = 0x80;
 
-        public Hit(int targetId, int damage, bool miss, bool crit, bool shld, bool soulshot, int grade)
+        public int TargetId { get; }
+        public int Damage { get; }
+        public int Flags { get; }
+
+        public bool IsMiss { get; }
+        public bool IsCritical { get; }
+        public bool IsSoulshotUsed { get; }
+        public int SoulshotGrade { get; }
+        public int Shield { get; }
+
+        public Hit(int targetId, int damage, bool isMiss, bool isCritical, int shield, bool isSoulshotUsed, int soulshotGrade)
         {
             TargetId = targetId;
             Damage = damage;
-            if (soulshot)
-                Flags |= 0x10 | grade;
+            IsMiss = isMiss;
+            IsCritical = isCritical;
+            Shield = shield;
+            IsSoulshotUsed = isSoulshotUsed;
+            SoulshotGrade = soulshotGrade;
 
-            if (crit)
-                Flags |= 0x20;
-            if (shld)
-                Flags |= 0x40;
-            if (miss)
-                Flags |= 0x80;
+            if (isMiss)
+            {
+                Flags = HITFLAG_MISS;
+                return;
+            }
+
+            if (isSoulshotUsed)
+            {
+                Flags |= HITFLAG_USESS | soulshotGrade;
+            }
+
+            if (isCritical)
+            {
+                Flags |= HITFLAG_CRITICAL;
+            }
+
+            if (shield > 0)
+            {
+                Flags |= HITFLAG_SHIELD;
+            }
         }
     }
 }
